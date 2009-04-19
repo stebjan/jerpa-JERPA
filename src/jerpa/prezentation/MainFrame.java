570a682d8bce4fa1394dd888fdf2d054353b61e2
vsouhrada <vsouhrada@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2009-04-19 11:57:48 +0000

    New images and updated mainframe
    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk/JERPA@10 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

--- src/jerpa/prezentation/MainFrame.java
+++ src/jerpa/prezentation/MainFrame.java
@@ -1,11 +1,59 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    MainFrame.java
+ *    Copyright (C) 2009 University of West Bohemia, 
+ *                       Department of Computer Science and Engineering, 
+ *                       Pilsen, Czech Republic
+ */
 package jerpa.prezentation;
 
 import ch.ethz.origo.jerpaui.ui.JERPAFrame;
+import ch.ethz.origo.jerpaui.ui.utils.listeners.AppButtonsEvent;
+import ch.ethz.origo.jerpaui.ui.utils.listeners.AppButtonsListener;
 
-public class MainFrame extends JERPAFrame {
+public class MainFrame implements AppButtonsListener {
 
-	JERPAFrame f = new JERPAFrame();
+	private JERPAFrame mainFrame;
+	
 	public MainFrame() {
-		super();
+		initGui();
+	}
+
+	private void initGui() {
+		mainFrame = new JERPAFrame(ClassLoader.getSystemResourceAsStream("jerpa/data/images/Jerpa_icon.png"));
+		mainFrame.setVisible(true);
+	}
+
+	@Override
+	public void closeAppButton(AppButtonsEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maximalizeAppButton(AppButtonsEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void minimalizelizeAppButton(AppButtonsEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }

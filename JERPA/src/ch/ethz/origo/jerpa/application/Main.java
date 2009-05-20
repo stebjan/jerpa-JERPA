15e2499fbee3123bfaf3321d3159435b3111ce52
vsouhrada <vsouhrada@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2009-05-20 04:42:48 +0000

    Update class Main.java. Added loading of MainFrame class to EventDispatchThread
    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk@18 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

--- JERPA/src/ch/ethz/origo/jerpa/application/Main.java
+++ JERPA/src/ch/ethz/origo/jerpa/application/Main.java
@@ -1,11 +1,36 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    Main.java
+ *    Copyright (C) 2009 University of West Bohemia, 
+ *                       Department of Computer Science and Engineering, 
+ *                       Pilsen, Czech Republic
+ */
 package ch.ethz.origo.jerpa.application;
+
+import javax.swing.SwingUtilities;
 
 import ch.ethz.origo.jerpa.prezentation.MainFrame;
 /**
  * 
  * @author Vaclav Souhrada (v.souhrada@gmail.com)
- * @version 0.1.0
- * @since 2.0
+ * @version 0.1.0 
+ * Date : 04/16/2009
+ * @since 1.0 (app. version)
  *     
  */
 public class Main {
@@ -14,7 +39,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new MainFrame();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MainFrame();				
+			}
+		});
 	}
-
 }

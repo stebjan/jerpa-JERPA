67a2a54f639675ff30a3d2bc166985a426d90784
vsouhrada <vsouhrada@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2009-10-21 13:16:18 +0000

    Added a new classes for project for each perspective in JERPA soft.
    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk@36 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

---
+++ JERPA/src/ch/ethz/origo/jerpa/application/perspective/signalprocess/project/SignalProcessProject.java
@@ -0,0 +1,28 @@
+package ch.ethz.origo.jerpa.application.perspective.signalprocess.project;
+
+import ch.ethz.origo.jerpa.application.project.IProjectOriginator;
+import ch.ethz.origo.jerpa.application.project.Project;
+import ch.ethz.origo.jerpa.application.project.ProjectMementoCaretaker;
+
+/**
+ * 
+ * @author Vaclav Souhrada (v.souhrada@gmail.com)
+ * @version 0.1.0 10/21/2009 
+ * @since 0.1.0 (06/07/2009)
+ * @see Project    
+ */
+public class SignalProcessProject extends Project {
+
+	@Override
+	public ProjectMementoCaretaker createMemento() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setMemento(ProjectMementoCaretaker memento) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JERPA/src/ch/ethz/origo/jerpa/application/project/Caretaker.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ch.ethz.origo.jerpa.application.project;
+
+/**
+ * 
+ * @author Vaclav Souhrada (v.souhrada@gmail.com)
+ * @version 0.1.0 10/21/2009 
+ * @since 0.1.0 (06/07/2009)
+ *     
+ */
+public interface Caretaker {
+
+	/**
+	 * Return class which represents memento state of project.
+	 * 
+	 * @return project memento state Class
+	 */
+	public Project getState();
+}

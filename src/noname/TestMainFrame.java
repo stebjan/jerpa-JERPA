24b981421712a4d7a0cc0309c00af9376855d53c
vsouhrada <vsouhrada@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2009-11-17 13:07:25 +0000

    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk/JERPA@39 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

--- src/noname/TestMainFrame.java
+++ src/noname/TestMainFrame.java
@@ -2,18 +2,51 @@ package noname;
 
 import java.io.InputStream;
 
+import ch.ethz.origo.jerpa.jerpalang.LangUtils;
+import ch.ethz.origo.juigle.application.exceptions.PerspectiveException;
 import ch.ethz.origo.juigle.prezentation.JUIGLEFrame;
+import ch.ethz.origo.juigle.prezentation.JUIGLEMainMenu;
 
 public class TestMainFrame extends JUIGLEFrame {
-
-	public TestMainFrame(String title, InputStream logoImg) {
-		super(title, logoImg);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	/** Only for serialization */
 	private static final long serialVersionUID = -1903614339597274647L;
+
+	public TestMainFrame() {
+		super();
+		StringBuffer titleBuff = new StringBuffer();
+		titleBuff.append(ConfigPropertiesLoader.getApplicationTitle());
+		titleBuff.append(" ");
+		titleBuff.append(ConfigPropertiesLoader.getAppMajorVersion());
+		titleBuff.append(".");
+		titleBuff.append(ConfigPropertiesLoader.getAppMinorVersion());
+		titleBuff.append(".");
+		titleBuff.append(ConfigPropertiesLoader.getAppRevisionVersion());
+		this.setTitle(titleBuff.toString());
+		this.setLogo(ClassLoader
+				.getSystemResourceAsStream("ch/ethz/origo/jerpa/data/images/Jerpa_icon.png"));
+		try {
+			this.addMainMenu(getMainMenu());
+			this.setPerspectives(PerspectiveLoader.getInstance());
+		} catch (PerspectiveException e) {
+			e.printStackTrace();
+		}
+		this.setVisible(true);	
+		
+	}
+
 	
+	public TestMainFrame(String title, InputStream logoImg) {
+		super(title, logoImg);
+	}
+	
+
+	private JUIGLEMainMenu getMainMenu() throws PerspectiveException {
+		JUIGLEMainMenu mainMenu = new JUIGLEMainMenu();
+		mainMenu.setLocalizedResourceBundle(LangUtils.MAIN_FILE_PATH);
+		mainMenu.addHomePageItem(null, "http://jerpa.origo.ethz.ch/");
+		return mainMenu;
+	}
 	
 
 }

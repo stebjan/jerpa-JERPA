3c15383faa38fcf8f1a31239cae5f890f4be7f69
vsouhrada <vsouhrada@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2010-02-08 21:40:20 +0000

    Implemented almost of JUIGLETreeTable to Filtering Manager (In the future the Filtering Manager will be changed to Algorithms Manager)
    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk/JERPA@67 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

--- src/ch/ethz/origo/jerpa/prezentation/perspective/FilterPerspective.java
+++ src/ch/ethz/origo/jerpa/prezentation/perspective/FilterPerspective.java
@@ -1,10 +1,6 @@
 package ch.ethz.origo.jerpa.prezentation.perspective;
 
 import java.awt.BorderLayout;
-import java.lang.reflect.Field;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.Vector;
 
 import javax.swing.Icon;
 import javax.swing.JScrollPane;
@@ -17,11 +13,11 @@ import org.jdesktop.swingx.treetable.DefaultMutableTreeTableNode;
 
 import ch.ethz.origo.jerpa.data.perspective.filter.FilterRecord;
 import ch.ethz.origo.jerpa.data.perspective.filter.FilterTreeTableModel;
+import ch.ethz.origo.juigle.application.exception.DataStoreException;
 import ch.ethz.origo.juigle.application.exception.JUIGLEMenuException;
 import ch.ethz.origo.juigle.application.exception.PerspectiveException;
 import ch.ethz.origo.juigle.application.observers.IObservable;
 import ch.ethz.origo.juigle.application.observers.IObserver;
-import ch.ethz.origo.juigle.data.ClassFinder;
 import ch.ethz.origo.juigle.data.tables.model.JUIGLETreeTableModel;
 import ch.ethz.origo.juigle.prezentation.JUIGLEGraphicsUtils;
 import ch.ethz.origo.juigle.prezentation.JUIGLEMenu;
@@ -33,7 +29,7 @@ import ch.ethz.origo.juigle.prezentation.tables.JUIGLETreeTable;
  * 
  * 
  * @author Vaclav Souhrada (v.souhrada@gmail.com)
- * @version 0.1.0 08/14/09
+ * @version 0.1.1 (2/08/2010)
  * @since 0.1.0 (05/18/09)
  * @see Perspective
  * @see IObserver
@@ -87,12 +83,12 @@ public class FilterPerspective extends Perspective implements IObserver {
 	public void initPerspectivePanel() throws PerspectiveException {
 		super.initPerspectivePanel();
 		mainPanel.setLayout(new BorderLayout());
-		JUIGLETreeTableModel ttm = new FilterTreeTableModel(fillByValues());
-		/*try {
+		JUIGLETreeTableModel ttm = new FilterTreeTableModel();
+		try {
 			ttm.fillByValues();
 		} catch (DataStoreException e) {
 			throw new PerspectiveException(e);
-		}*/
+		}
 		JUIGLETreeTable table = new JUIGLETreeTable(ttm);
 		table.setEditable(false);
 		table.setTreeCellRenderer(new DefaultTreeCellRenderer() {
@@ -124,64 +120,6 @@ public class FilterPerspective extends Perspective implements IObserver {
 		mainPanel.add(sp, BorderLayout.CENTER);
 	}
 	
-	//@Override
-	public DefaultMutableTreeTableNode fillByValues() {
-		Set<FilterRecord> treeOfFilters = new TreeSet<FilterRecord>();
-		ClassFinder cf = new ClassFinder();
-		Vector<Class<?>> listOfSubclasses = cf
-				.findSubclasses("ch.ethz.origo.jerpa.data.filters.IAlgorithmDescriptor");
-		for (Class<?> item : listOfSubclasses) {
-			try {
-				FilterRecord fr = new FilterRecord();
-				Field author = item.getDeclaredField("AUTHOR");
-				Field version = item.getDeclaredField("VERSION");
-				Field name = item.getDeclaredField("NAME");
-				Field category = item.getDeclaredField("CATEGORY");
-				author.setAccessible(true);
-				version.setAccessible(true);
-				name.setAccessible(true);
-				category.setAccessible(true);
-				fr.setAuthor((String) author.get(null));
-				fr.setName((String) name.get(null));
-				fr.setVersion((String) version.get(null));
-				fr.setCategory((String) category.get(null));
-				treeOfFilters.add(fr);
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (NoSuchFieldException e) {
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		String prevLast = "";
-		String currentLast = "";
-
-		FilterTreeTableNode currentNameNode = null;
-		DefaultMutableTreeTableNode root = new FilterTreeTableNode(
-				new FilterRecord());
-		for (FilterRecord item : treeOfFilters) {
-			currentLast = item.getCategory();
-			if (currentLast.equals(prevLast)) {
-				currentNameNode.add(new FilterTreeTableNode(item));
-			} else {
-				if (currentNameNode != null) {
-					root.add(currentNameNode);
-				}
-				FilterRecord fr = new FilterRecord();
-				fr.setCategory(item.getCategory());
-				currentNameNode = new FilterTreeTableNode(fr);
-				currentNameNode.add(new FilterTreeTableNode(item));
-				prevLast = currentLast;
-				root.add(currentNameNode);
-			}
-		}
-		return root;
-	}
-
-	
 	@Override
 	public String getResourceBundlePath() {
 		return FilterPerspective.resourcePath;
@@ -211,35 +149,6 @@ public class FilterPerspective extends Perspective implements IObserver {
 	@Override
 	public void update(IObservable o, Object state) {
 		// TODO Auto-generated method stub
-		
-	}
-	
-	/**
-	 * 
-	 * @author Vaclav Souhrada
-	 * @version 0.1.0 (7/08/2010)
-	 * @since 0.2.0 (7/08/2010)
-	 * @see DefaultMutableTreeTableNode
-	 * 
-	 */
-	class FilterTreeTableNode extends DefaultMutableTreeTableNode {
-
-		public FilterTreeTableNode(FilterRecord record) {
-			super(record);
-		}
-
-		@Override
-		public Object getValueAt(int column) {
-			Object toBeDisplayed = "nn";
-
-			return toBeDisplayed;
-		}
-		
-		@Override
-		public int getColumnCount() {
-			return 3;
-		}
-		
 		
 	}
 		

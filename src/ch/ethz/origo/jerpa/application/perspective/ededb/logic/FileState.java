5e2a13f0837f1c82174ec60afdfef5c515ac3d32
kivan <kivan@f78101ed-1b90-4e26-8a18-f1c6f94e06c0>
2011-09-03 16:35:22 +0000

    ADDED: java files that should be in previous commit
    git-svn-id: https://svn.origo.ethz.ch/jerpa/JERPA/trunk/JERPA@236 f78101ed-1b90-4e26-8a18-f1c6f94e06c0

---
+++ src/ch/ethz/origo/jerpa/application/perspective/ededb/logic/FileState.java
@@ -0,0 +1,30 @@
+package ch.ethz.origo.jerpa.application.perspective.ededb.logic;
+
+/**
+ * Enum for description of file's download state.
+ *
+ * @author Petr Miko - miko.petr (at) gmail.com
+ *
+ */
+public enum FileState {
+
+	/**
+	 * File has not been downloaded yet.
+	 */
+	NO_COPY,
+
+	/**
+	 * File is successfully downloaded.
+	 */
+	HAS_COPY,
+
+	/**
+	 * File was being downloaded but is not complete (size)
+	 */
+	CORRUPTED,
+
+	/**
+	 * File is dowloading right now.
+	 */
+	DOWNLOADING
+}
---
+++ src/ch/ethz/origo/jerpa/application/perspective/ededb/tables/DataRowSorter.java
@@ -0,0 +1,43 @@
+package ch.ethz.origo.jerpa.application.perspective.ededb.tables;
+
+import java.util.Comparator;
+
+import javax.swing.table.TableRowSorter;
+
+/**
+ * Sorter for tables (otherwise exceptions in DataTableModel - default compare
+ * does not handle enums)
+ *
+ * @author Petr Miko - miko.petr (at) gmail.com
+ *
+ */
+public class DataRowSorter extends TableRowSorter<DataTableModel> {
+
+	/**
+	 * Constructor method.
+	 *
+	 * @param dataModel table Model
+	 */
+	public DataRowSorter(DataTableModel dataModel) {
+		super(dataModel);
+	}
+
+	@Override
+	public Comparator<?> getComparator(int column) {
+		return new Comparator<Object>() {
+
+			@Override
+			public int compare(Object o1, Object o2) {
+
+				if (o1 instanceof Number && o2 instanceof Number)
+					return (Integer) o1 - (Integer) o2;
+				else {
+					// copied from java tutorials - modified with toString to prevent conversion issues
+					String[] strings1 = o1.toString().split("\\s");
+					String[] strings2 = o2.toString().split("\\s");
+					return strings1[strings1.length - 1].compareTo(strings2[strings2.length - 1]);
+				}
+			}
+		};
+	}
+}
